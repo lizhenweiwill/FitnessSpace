8bc27c0e7b281ef16f53be85d7a5d402f7fb9f92
lizhenwei.will <lizhenwei.will@foxmail.com>
2022-03-10 22:33:37 +0800

    sync.Once

--- provider/provider.go
+++ provider/provider.go
@@ -1,1 +1,21 @@
 package provider
+
+import "sync"
+
+type Database struct {
+	dsn string
+}
+
+func (db *Database) Use() {
+	println("数据库被使用了")
+}
+
+var once sync.Once
+var db *Database
+
+func NewDB() *Database {
+	once.Do(func() {
+		db = &Database{}
+	})
+	return db
+}
