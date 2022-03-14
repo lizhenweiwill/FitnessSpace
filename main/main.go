1c6ea17e0a5dbcbff3dae5309b61c38fc4a6c664
lizhenwei.will <lizhenwei.will@foxmail.com>
2022-03-14 17:07:14 +0800

    add API

--- main/main.go
+++ main/main.go
@@ -1,6 +1,29 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
-	db := InitDB()
-	db.Use()
+	engine := gin.Default()
+	engine.Group("/api/v1")
+	{
+		// 教练相关
+		engine.GET("/coach/:id")
+		engine.POST("/coach")
+		engine.DELETE("/coach")
+		// 课程相关
+		engine.GET("/course/:id")
+		engine.POST("/course")
+		engine.DELETE("/course")
+		// 会员相关
+		engine.GET("/member/:id")
+		engine.POST("/member")
+		engine.PUT("/member") // 修改信息
+		// 记录相关
+		engine.POST("/record")
+		engine.PUT("/record")
+	}
+	err := engine.Run(":8080")
+	if err != nil {
+		panic("服务启动失败！")
+	}
 }
